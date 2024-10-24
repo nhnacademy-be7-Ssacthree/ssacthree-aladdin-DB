6416eeb87c91ebab3da39c76406c5c0112bf44b1
최유현 <dbgus315@naver.com>
2024-10-24 15:49:55 +0900

    feat: 알라딘 open API로부터 책의 정보를 받아오는 코드

---
+++ src/main/java/com/nhnacademy/ssackthree_aladdin_DB/domain/APIData.java
@@ -0,0 +1,47 @@
+package com.nhnacademy.ssackthree_aladdin_DB.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "api_data")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class APIData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long bookId;
+
+    @NotNull
+    private String title;
+    @NotNull
+    private String author;
+    @NotNull
+    private LocalDate pubDate;
+    @NotNull
+    private String description;
+    @NotNull
+    private String isbn13;
+    @NotNull
+    private int priceSales;//판매가
+    @NotNull
+    private int priceStandard; // 정가
+    @NotNull
+    private String cover; //URL
+    @NotNull
+    private String categoryName;
+    @NotNull
+    private String publisher;
+
+}
---
+++ src/main/java/com/nhnacademy/ssackthree_aladdin_DB/repository/APIRepository.java
@@ -0,0 +1,10 @@
+package com.nhnacademy.ssackthree_aladdin_DB.repository;
+
+import com.nhnacademy.ssackthree_aladdin_DB.domain.APIData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface APIRepository extends JpaRepository<APIData, String> {
+    boolean existsByIsbn13(String isbn13);
+}
